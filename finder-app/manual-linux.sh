9d47637e5fb634cec651078bc1be87743e84eca9
Youssef-74 <yosifesam98@gmail.com>
2024-03-09 04:05:45 +0200

    resubmit assignment-3 part-2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -100,9 +100,9 @@ make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
 
 cd ${OUTDIR}/rootfs
 
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+#echo "Library dependencies"
+#${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+#${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
 # Find the toolchain sysroot path dynamically
